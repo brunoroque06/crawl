307054140577bf5b51a0f8d3c34a4b64e198c519
Bruno Roque <brunoroque06@gmail.com>
2026-06-02 11:30:21 +0200

    Add single digit day time parsing

--- time.go
+++ time.go
@@ -12,7 +12,13 @@ func (t *DesTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	if err := d.DecodeElement(&s, &start); err != nil {
 		return err
 	}
-	fmts := [...]string{time.RFC1123, time.RFC1123Z, time.RFC3339}
+	fmts := [...]string{
+		time.RFC1123,
+		time.RFC1123Z,
+		time.RFC3339,
+		"Mon, 2 Jan 2006 15:04:05 MST",   // RFC1123 with single digit day
+		"Mon, 2 Jan 2006 15:04:05 -0700", // RFC1123Z "
+	}
 	for _, f := range fmts {
 		parsed, err := time.Parse(f, s)
 		if err == nil {
